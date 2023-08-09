1480ffe4ae2d6b1107c44275688ebc99a2a9cd8a
BooDwarf <127206991+BooDwarf@users.noreply.github.com>
2023-08-08 22:12:06 -0300

    Background added
    * Added an image background
    * Added background scroller script (looping background image)
    * Added regular flappy bird assests to modify later
    * Added pipes (need to make pipe logic and physics. Also need to modify  their textures)

---
+++ Flappy Bird Copy/Assets/backgroundScroller.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class backgroundScroller : MonoBehaviour
+{
+
+    [Range(-1f, 1f)]
+    public float scrollSpeed = 0.08f;
+    private float offset;
+    private Material mat;
+
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+    void Update()
+    {
+        offset = Time.time * scrollSpeed;
+        mat.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+
+    }
+}
